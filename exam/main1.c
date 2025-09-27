dc2aca64de31bdbbdebd2e5b51e8730920a725e3
Hedwig0625 <hedwigchen060625@gmail.com>
2025-09-27 14:31:40 +0800

    Update main1.c

--- exam/main1.c
+++ exam/main1.c
@@ -1,3 +1,8 @@
+//after providing a number i,takes input for mileage i and calcuate the taxi.
+//Assuming the fare is 70 dollars for milleage below 1500 meters
+//an addtional 10 dollars for every 100 meters over 1600
+//with distance less than 100 meters counted as 100 meters.
+
 #include <stdio.h>
 
 int main(){
