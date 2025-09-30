d5c8eb9497d07444b25ab1ef8b4f9ee90320d9f4
Hedwig0625 <hedwigchen060625@gmail.com>
2025-09-30 17:22:18 +0800

    Update main7.c

--- exam/main7.c
+++ exam/main7.c
@@ -11,3 +11,39 @@
 //*     *
 //*******
 
+#include <stdio.h>
+
+int main(){
+    
+    int n=4;
+    
+    for(int i=0; i<n; i++){
+        
+        for(int j=0;j<n-i-1;j++){
+            printf(" ");
+        }
+        
+        for(int k=0;k < 2*i+1 ;k++){
+            printf("*");
+        }
+        
+    printf("\n");    
+    }
+    
+    int row= n+2;
+    int col=2*n-1;
+    
+    for(int l=0;l<row;l++){
+        
+        for(int m=0;m<col;m++) {
+            if(l == 0|| l == row -1 ||m ==0 || m == col-1){
+                printf("*");    
+            }else{
+                printf(" ");   
+            }
+        }   
+    printf("\n");
+    }
+    
+    return 0;
+}
